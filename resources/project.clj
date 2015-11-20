eb070739e5400798133e7c77b2a8a35b6ce0c7d6
Artur Girenko <a.girenko@gmail.com>
2015-11-20 20:07:42 +0100

    Add .gitignore

--- resources/project.clj
+++ resources/project.clj
@@ -7,7 +7,7 @@
                  [org.clojure/clojurescript "1.7.145"]
                  [$REACT_INTERFACE$]
                  [org.omcljs/ambly "0.6.0"]
-                 [natal-shell "0.1.0"]]
+                 ]
  :plugins [[lein-cljsbuild "1.1.0"]]
  :cljsbuild {:builds {:dev {:source-paths ["src"]
                             :compiler {:output-to "target/out/main.js"
